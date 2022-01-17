cbe15c475641590756d40cde64b626eb78d7ca3f
Theophilus Awoyele <theophilusawoyele@gmail.com>
2022-01-17 23:31:13 +0000

    final touches

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
     $(window).scroll(function(){       
-        // scroll-up button show/hide script
         if(this.scrollY > 500){
             $('.scroll-up-btn').addClass("show");
         }else{
@@ -8,16 +7,12 @@ $(document).ready(function(){
         }
     });
     $('.nav .nav-menu li a').click(function(){
-        // applying again smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
     });
-// toggle menu/navbar script
     $('.nav-btn').click(function(){
         $('.nav .nav-menu').toggleClass("active");
         $('.nav-btn i').toggleClass("active");
     });
-
-    // typing text animation script
     let typed = new Typed(".display", {
         strings: ["Software Engineer", "Full stack developer", "Freelancer", "GIS specialist"],
         typeSpeed: 60,
